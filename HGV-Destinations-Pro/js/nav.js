a6e2669c8ded8bb1704464bf6915f8c6890df299
twmiddleton21-lgtm <twmiddleton21@gmail.com>
2026-02-24 22:05:30 +0000

    Delete HGV-Destinations-Pro directory

--- HGV-Destinations-Pro/js/nav.js
+++
@@ -1,7 +0,0 @@
-export const NAV = {
-  setActive(route){
-    document.querySelectorAll(".navBtn").forEach(btn=>{
-      btn.classList.toggle("active", btn.dataset.route === route);
-    });
-  }
-};
